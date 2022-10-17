cc2b3d20dabb541a31d93a2c33fa6ff362902316
Vikkyn <116003516+Vikkyn@users.noreply.github.com>
2022-10-18 02:38:59 +0300

    Create Classes.py

---
+++ Classes.py
@@ -0,0 +1,37 @@
+class Coffee:
+  def __init__(self, country: str, roast: str, taste: str, weight: int, cost: int):
+    """
+    Создание и подготовка к работе объекта "Кофе"
+    
+    :param country: Страна
+    :param roast: Обжарка
+    :param taste: Вкус
+    :param weight: Вес
+    :param cost: Стоимость
+    """
+    self.country = country
+    self.roast = roast
+    self.taste = taste
+    
+  def get_info_coffee(self):
+    """
+    Функция которая выдает информацию о параметрах сорта кофе
+    
+    """
+    ...
+    
+  def choose_a_coffee(self):
+    """
+    Функция которая помогает выбрать кофе   
+    
+    """
+    #???????
+    ...
+    
+  def calculate_price(self, weight * cost):
+    """
+    Функция которая рассчитывает стоимость кофе исходя из веса
+    
+    """
+    ...
+  
